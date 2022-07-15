f82fe058d171c2ccf71b5d62e666f1fa68411584
jdamonew <damone556@gmail.com>
2022-07-15 15:30:01 -0300

    test 2 github pages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,7 +8,7 @@ import Home from "../Pages/Home"
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
