8eace1d715e490bbd0bab2cfefa8887b674c1fd3
jdamonew <damone556@gmail.com>
2022-07-16 08:37:37 -0300

    update: add responsivity

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,25 @@
 import React from "react";
-import { Grid, Row, Col } from "rsuite";
 import Logo from "../Components/Logo"
 import Socials from "../Components/Socials";
 import "../Styles/Home.scss";
 
-const Home = () =>{
-    return(
+const Home = () => {
+    return (
         <div className="grid-home" >
-            <Grid fluid>
-                <Row className="header-row-home">
-                    <Col>
-                        <Logo />
-                    </Col>
-                </Row>
-                <Row className="body-row-home">
-                    <Col xs={24} sm={24} md={24}>
-                        <h1 className="home-msg">coming soon...</h1>
-                        <Socials/>
-                    </Col>
-                </Row>
-                <Row className="footer-row-home">
-                    <Col xs={24} sm={24} md={24}>
-                        <span className="home-msg"> 
-                            Copywrite © 2022 Jóbia Damone
-                        </span>
-                    </Col>
-                </Row>
-            </Grid>
+            <header className="header-row-home">
+                <Logo />
+            </header>
+            <div className="body-row-home">
+                <h2 className="home-msg">coming soon...</h2>
+                <Socials />
+            </div>
+            <footer className="footer-row-home">
+                <span className="home-msg">
+                    Copywrite © 2022 Jóbia Damone
+                </span>
+            </footer>
         </div>
-                
+
     )
 }
 
