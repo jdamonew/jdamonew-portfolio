4e1f91d98489b619314c47e6466b30f47b45f18e
Jóbia Damone <jobiadamone@hotmail.com>
2022-07-18 18:10:53 -0300

    settings: add colors settings

--- src/Components/Socials/index.jsx
+++ src/Components/Socials/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { socials } from "../../Configs/Socials";
+import { socials } from "../../Configs/Params";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "./styles.scss";
 
 const Socials = () =>{
