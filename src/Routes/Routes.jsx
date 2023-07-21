fccf06ee8b8ed7656a0a7e1f3148b1123eeb483c
Jobia Damone <jobiadamone@hotmail.com>
2023-07-21 14:36:42 -0300

    added posthog implamentation

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,10 +5,17 @@ import {
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../Store"
+import posthog from 'posthog-js'
+
 
 import Home from "../Pages/Home"
 
 function App() {
+  
+  posthog.init('phc_dkTb8A3vzQILwk26wUJ7vSSJ5zUgVnyGqVNbmba1O7a', { 
+    api_host: 'https://app.posthog.com' 
+  })
+  
   return (
     <Provider store={store}>
       <BrowserRouter>
