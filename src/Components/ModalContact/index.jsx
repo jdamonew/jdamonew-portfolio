b5fe9bea32690b8a2800c741d2851384322e8a3a
Jobia Damone <jobiadamone@hotmail.com>
2025-08-31 10:52:12 -0300

    posthog: adicionado trackeamento nos links de contato

--- src/Components/ModalContact/index.jsx
+++ src/Components/ModalContact/index.jsx
@@ -1,16 +1,22 @@
-import React, { useRef } from "react";
-
+import { useRef } from "react";
 import { X, AtSign, Linkedin, SquareArrowOutUpRight, MessageCircle } from "lucide-react";
 import { contacts } from "../../Configs/Params";
 import Button from "../General/Button";
 import { Whatsapp } from "iconsax-react";
 import PropTypes from "prop-types";
+import posthog from "posthog-js";
 
 const ModalContact = ({ text, data }) => {
     const modalRef = useRef();
 
     const openModal = () => modalRef.current?.showModal();
     const closeModal = () => modalRef.current?.close();
+
+    function redirect(nameSocial) {
+        posthog.capture('contacts_open', {
+            socialName: nameSocial
+        })
+    }
 
     return (
         <>
@@ -29,14 +35,18 @@ const ModalContact = ({ text, data }) => {
                         <p>{data.modalMessage}</p>
                         <div className="contacts_item">
                             <AtSign size="30" />
-                            <a href={`mailto:${contacts.email}`}>{contacts.email}
-
+                            <a
+                                onClick={() => redirect('email')}
+                                href={`mailto:${contacts.email}`}
+                            >
+                                {contacts.email}
                                 <SquareArrowOutUpRight size={15} />
                             </a>
                         </div>
                         <div className="contacts_item">
                             <Whatsapp size="30" />
                             <a
+                                onClick={() => redirect('whatsapp')}
                                 href={`https://api.whatsapp.com/send?phone=5581996126534&text=Ol%C3%A1,%20estou%20vindo%20do%20seu%20site!%20Podemos%20conversar?`}
                             >
                                 {contacts.phone}
@@ -46,6 +56,7 @@ const ModalContact = ({ text, data }) => {
                         <div className="contacts_item">
                             <Linkedin size="30" />
                             <a
+                                onClick={() => redirect('linkedin')}
                                 href={`https://www.linkedin.com/${contacts.linkedin}-a376b0177/`}
                             >
                                 {contacts.linkedin}
